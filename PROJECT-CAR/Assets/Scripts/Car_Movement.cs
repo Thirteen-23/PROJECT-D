20025fa2df82204709dd97132246a51e6fb530af
Thirteen-23 <gabriel.ubay@gmail.com>
2024-04-21 22:28:01 +1000

    WIP new control system for the car
    implementation of unity input system.

--- PROJECT-CAR/Assets/Scripts/Car_Movement.cs
+++ PROJECT-CAR/Assets/Scripts/Car_Movement.cs
@@ -4,26 +4,32 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Xml;
 using System.Xml.Serialization;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Car_Movement : MonoBehaviour
 {
+    private Vector2 m_PlayerMovement = Vector2.zero;
+    private InputAction m_Movement; 
+    private PlayerInput carNewInputSystem;
+    
 
-   enum DifferentialTypes
-        {
+    enum DifferentialTypes
+    {
         FrontWheelDrive,
         RearWheelDrive,
         AllWheelDrive
 
-        }
-    [SerializeField] private DifferentialTypes drive; 
-   [SerializeField] WheelCollider[] wheels4 = new WheelCollider[4];
+    }
+    [SerializeField] private DifferentialTypes drive;
+    [SerializeField] WheelCollider[] wheels4 = new WheelCollider[4];
     [SerializeField] GameObject[] wheelmeshes = new GameObject[4];
     [SerializeField] AnimationCurve enginePower;
-   // [SerializeField] private WheelFrictionCurve sRFriction;
+    // [SerializeField] private WheelFrictionCurve sRFriction;
 
-
-
+    float m_steering; 
+  
     float horizontalInput;
     float verticalInput;
     float currentSteerAngle, currentBreakForce, handbraking;
@@ -65,48 +71,62 @@ public class Car_Movement : MonoBehaviour
     [Header("Wheel Modifiers")]
     [SerializeField] private float engineTorque;
     //[SerializeField] private float frontEngineTorque;
-  // [SerializeField] private float rearEngineTorque;
-   [SerializeField] private float breakForce;
-   [SerializeField] private float frontBreakForce;
-   [SerializeField] private float rearBreakForce;
+    // [SerializeField] private float rearEngineTorque;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float frontBreakForce;
+    [SerializeField] private float rearBreakForce;
     [SerializeField] private float maxSteerAngle;
     #region old wheel collder and transform code
     [Header("Wheel Collider and Transform")]
 
-   /* [SerializeField]*/ private WheelCollider frontRightWheelCollider;
-   /* [SerializeField]*/ private WheelCollider frontLeftWheelCollider;
-   /* [SerializeField]*/ private WheelCollider rearRightWheelCollider;
-   /* [SerializeField]*/ private WheelCollider rearLeftWheelCollider;
+    /* [SerializeField]*/
+    private WheelCollider frontRightWheelCollider;
+    /* [SerializeField]*/
+    private WheelCollider frontLeftWheelCollider;
+    /* [SerializeField]*/
+    private WheelCollider rearRightWheelCollider;
+    /* [SerializeField]*/
+    private WheelCollider rearLeftWheelCollider;
 
-   /* [SerializeField]*/ private Transform frontRightWheelTransform;
-   /* [SerializeField]*/ private Transform frontLeftWheelTransform;
-   /* [SerializeField]*/ private Transform rearRightWheelTransform;
-   /* [SerializeField]*/ private Transform rearLeftWheelTransform;
+    /* [SerializeField]*/
+    private Transform frontRightWheelTransform;
+    /* [SerializeField]*/
+    private Transform frontLeftWheelTransform;
+    /* [SerializeField]*/
+    private Transform rearRightWheelTransform;
+    /* [SerializeField]*/
+    private Transform rearLeftWheelTransform;
     #endregion
 
     // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         originalPos = gameObject.transform.position;
         rotations = gameObject.transform.rotation;
         brakeTrailRenLeft = brakeTrailLeft.GetComponent<TrailRenderer>();
         brakeTrailRenRight = brakeTrailRight.GetComponent<TrailRenderer>();
-
+        carNewInputSystem = GetComponent<PlayerInput>();
+        m_Movement = carNewInputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     private void FixedUpdate()
     {
+        
         GettingInput();
         HandlingMotor();
         HandlingSteering();
         AnimatedWheels();
-
+        Charactermove();
         quitApplication();
         ResettingCar();
         BrakesUsed();
 
-      
+
 
         #region old code
 
@@ -156,34 +176,34 @@ public class Car_Movement : MonoBehaviour
             #region New Driving system
             if (drive == DifferentialTypes.AllWheelDrive)
             {
-                for(int i = 0; i <wheels4.Length; i++)
+                for (int i = 0; i < wheels4.Length; i++)
                 {
-                    wheels4[i].motorTorque = verticalInput * (engineTorque / 4);
+                    wheels4[i].motorTorque = verticalInput + m_PlayerMovement.y * (engineTorque / 4);
                 }
             }
-            else if( drive == DifferentialTypes.RearWheelDrive)
+            else if (drive == DifferentialTypes.RearWheelDrive)
             {
-                for(int i= 2; i< wheels4.Length; i++)
+                for (int i = 2; i < wheels4.Length; i++)
                 {
-                    wheels4[i].motorTorque = verticalInput * (engineTorque / 2);
+                    wheels4[i].motorTorque = verticalInput + m_PlayerMovement.y * (engineTorque / 2);
                 }
             }
-            else if(drive == DifferentialTypes.FrontWheelDrive)
-           
+            else if (drive == DifferentialTypes.FrontWheelDrive)
+
             {
-                for(int i = 0; i < wheels4.Length - 2; i++)
+                for (int i = 0; i < wheels4.Length - 2; i++)
                 {
-                    wheels4[i].motorTorque = verticalInput * (engineTorque / 2); 
+                    wheels4[i].motorTorque = verticalInput + m_PlayerMovement.y * (engineTorque / 2);
                 }
             }
 
             #endregion
 
             #region old Driving Force code
-              /*frontLeftWheelCollider.motorTorque = verticalInput * (engineTorque + frontEngineTorque) * speedFactor * Time.deltaTime;
-                frontRightWheelCollider.motorTorque = verticalInput * (engineTorque + frontEngineTorque) * speedFactor * Time.deltaTime;
-                rearLeftWheelCollider.motorTorque = verticalInput * (engineTorque + rearEngineTorque) * speedFactor * Time.deltaTime;
-                rearRightWheelCollider.motorTorque = verticalInput * (engineTorque + rearEngineTorque) * speedFactor * Time.deltaTime;*/
+            /*frontLeftWheelCollider.motorTorque = verticalInput * (engineTorque + frontEngineTorque) * speedFactor * Time.deltaTime;
+              frontRightWheelCollider.motorTorque = verticalInput * (engineTorque + frontEngineTorque) * speedFactor * Time.deltaTime;
+              rearLeftWheelCollider.motorTorque = verticalInput * (engineTorque + rearEngineTorque) * speedFactor * Time.deltaTime;
+              rearRightWheelCollider.motorTorque = verticalInput * (engineTorque + rearEngineTorque) * speedFactor * Time.deltaTime;*/
 
             #endregion
         }
@@ -194,14 +214,14 @@ public class Car_Movement : MonoBehaviour
             {
                 for (int i = 0; i < wheels4.Length; i++)
                 {
-                    wheels4[i].motorTorque = verticalInput * 0;
+                    wheels4[i].motorTorque = horizontalInput + m_PlayerMovement.y * 0;
                 }
             }
             else if (drive == DifferentialTypes.RearWheelDrive)
             {
                 for (int i = 2; i < wheels4.Length; i++)
                 {
-                    wheels4[i].motorTorque = verticalInput * 0;
+                    wheels4[i].motorTorque = horizontalInput + m_PlayerMovement.y * 0;
                 }
             }
             else if (drive == DifferentialTypes.FrontWheelDrive)
@@ -209,7 +229,7 @@ public class Car_Movement : MonoBehaviour
             {
                 for (int i = 0; i < wheels4.Length - 2; i++)
                 {
-                    wheels4[i].motorTorque = verticalInput * 0;
+                    wheels4[i].motorTorque = horizontalInput + m_PlayerMovement.y * 0;
                 }
             }
 
@@ -230,7 +250,7 @@ public class Car_Movement : MonoBehaviour
 
     private void ApplyBreaking()
     {
-        for(int i = 0; i< wheels4.Length; i++)
+        for (int i = 0; i < wheels4.Length; i++)
         {
             wheels4[i].brakeTorque = currentBreakForce;
         }
@@ -247,7 +267,7 @@ public class Car_Movement : MonoBehaviour
 
     private void ApplyHandBraking()
     {
-        for(int i = 2; i < wheels4.Length; i++)
+        for (int i = 2; i < wheels4.Length; i++)
         {
             wheels4[i].brakeTorque = handbraking;
         }
@@ -261,38 +281,38 @@ public class Car_Movement : MonoBehaviour
     }
     private void HandlingSteering()
     {
-        for(int i = 0; i < wheels4.Length-2; i++)
+        for (int i = 0; i < wheels4.Length - 2; i++)
         {
-            wheels4[i].steerAngle = horizontalInput * maxSteerAngle;
+            wheels4[i].steerAngle = horizontalInput + m_PlayerMovement.x  * maxSteerAngle;
         }
 
         #region old Steering system
-      /*  currentSteerAngle = maxSteerAngle * horizontalInput;
-        frontLeftWheelCollider.steerAngle = currentSteerAngle;
-        frontRightWheelCollider.steerAngle = currentSteerAngle; */
+        /*  currentSteerAngle = maxSteerAngle * horizontalInput;
+          frontLeftWheelCollider.steerAngle = currentSteerAngle;
+          frontRightWheelCollider.steerAngle = currentSteerAngle; */
         #endregion
     }
 
     #region Old Updating code for updating animation on wheels 
-   /* private void WheelsUpdating()
-    {
-        UpdateOneWheel(frontLeftWheelCollider, frontLeftWheelTransform);
-        UpdateOneWheel(frontRightWheelCollider, frontRightWheelTransform);
-        UpdateOneWheel(rearLeftWheelCollider, rearLeftWheelTransform);
-        UpdateOneWheel(rearRightWheelCollider, rearRightWheelTransform);
-    }
+    /* private void WheelsUpdating()
+     {
+         UpdateOneWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+         UpdateOneWheel(frontRightWheelCollider, frontRightWheelTransform);
+         UpdateOneWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+         UpdateOneWheel(rearRightWheelCollider, rearRightWheelTransform);
+     }
 
-    private void UpdateOneWheel(WheelCollider collider, Transform wheelTransform)
-    {
-        Vector3 pos;
-        Quaternion rot;
-        collider.GetWorldPose(out pos, out rot);
-        wheelTransform.rotation = rot;
-        wheelTransform.position = pos;
+     private void UpdateOneWheel(WheelCollider collider, Transform wheelTransform)
+     {
+         Vector3 pos;
+         Quaternion rot;
+         collider.GetWorldPose(out pos, out rot);
+         wheelTransform.rotation = rot;
+         wheelTransform.position = pos;
 
 
-    }
-   */
+     }
+    */
     #endregion
 
     void AnimatedWheels()
@@ -300,7 +320,7 @@ public class Car_Movement : MonoBehaviour
         Vector3 wheelPosition = Vector3.zero;
         Quaternion wheelRotations = Quaternion.identity;
 
-        for(int i = 0; i< wheels4.Length; i++)
+        for (int i = 0; i < wheels4.Length; i++)
         {
             wheels4[i].GetWorldPose(out wheelPosition, out wheelRotations);
             wheelmeshes[i].transform.position = wheelPosition;
@@ -315,7 +335,7 @@ public class Car_Movement : MonoBehaviour
         AnimationCurve curve1 = new AnimationCurve();
         AnimationCurve curve2 = new AnimationCurve();
         //sRFriction = new WheelFrictionCurve();
-           
+
         float trailWidth = 1.0f;
         if (ifHandBraking == true)
         {
@@ -335,7 +355,7 @@ public class Car_Movement : MonoBehaviour
             curve1.AddKey(0.0f, changeTrailsizeBeginning);
             curve2.AddKey(1.0f, changeTrailsizeEnd);
             curve2.AddKey(0.0f, changeTrailsizeBeginning);
-           // sRFriction.asymptoteValue = 0.75f;
+            // sRFriction.asymptoteValue = 0.75f;
         }
         //rearRightWheelCollider.sidewaysFriction = sRFriction;
         brakeTrailRenLeft.time = trailTime;
@@ -348,27 +368,20 @@ public class Car_Movement : MonoBehaviour
         brakeTrailRenRight.widthMultiplier = trailWidth;
     }
 
-    private void CarSetUp()
-    {
-        foreach( WheelCollider Wheel in wheels4 )
-        {
-           
-        }
-
-    }
+   
 
     private void EngineRPMSystem()
     {
-        if(drive == DifferentialTypes.AllWheelDrive)
+        if (drive == DifferentialTypes.AllWheelDrive)
         {
-            for(int i = 0; i< wheels4.Length; i++)
+            for (int i = 0; i < wheels4.Length; i++)
             {
-                engineRPM = wheels4[i].rpm / 4 * gearRatio[currentGear]; 
+                engineRPM = wheels4[i].rpm / 4 * gearRatio[currentGear];
             }
         }
         else if (drive == DifferentialTypes.FrontWheelDrive)
         {
-            for (int i = 0; i < wheels4.Length -2; i++)
+            for (int i = 0; i < wheels4.Length - 2; i++)
             {
                 engineRPM = wheels4[i].rpm / 2;
             }
@@ -415,4 +428,28 @@ public class Car_Movement : MonoBehaviour
     //    }
     //    currentGear = theCorrectGear;
     //}
+
+    public void Charactermove()
+    {
+        // context.ReadValue<Vector2>();
+        // print(context);
+        m_PlayerMovement = m_Movement.ReadValue<Vector2>();
+        print(m_PlayerMovement); 
+
+    }
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    public void Steering(InputAction.CallbackContext context)
+    {
+        m_steering = context.ReadValue<float>();
+    }
+    
 }
